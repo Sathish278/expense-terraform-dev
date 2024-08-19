295b2a8c32f8e98cf7c2f0eb0a487a342035b342
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-08-20 02:44:16 +0530

    latest expense terrafrom dev version

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # user data will get sudo access
 dnf install ansible -y
+mkdir /tmp/sathish
 cd /tmp
 git clone https://github.com/Sathish278/expense-ansible-roles.git
-cd expense-ansible-roles
-ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
-ansible-playbook main.yaml -e component=frontend
+cd /tmp/expense-ansible-roles
+sudo ansible-playbook main.yml -e component=backend -e login_password=ExpenseApp1
+sudo ansible-playbook main.yml -e component=frontend
